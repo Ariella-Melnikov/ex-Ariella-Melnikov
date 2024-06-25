eb2fe841f2f4e05db277572b28d0c4ec59d084f0
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-06-25 06:50:56 +0300

    generate 3 random reviews for base data

--- react-template-proj/services/book-service.js
+++ react-template-proj/services/book-service.js
@@ -1,144 +1,172 @@
 import { utilService } from './util.service.js'
 import { storageService } from './async-storage.service.js'
-import {books as staticBooks} from './books.js'
+import { books as staticBooks } from './books.js'
 
 const BOOK_KEY = 'bookDB'
 
 export const bookService = {
-    query,
-    getById,
-    remove,
-    save,
-    getEmptyBook,
-    getNextBookId,
-    getFilterBy,
-    setFilterBy,
-    getDefaultFilter,
-    addReview,
-    deleteReview,
+  query,
+  getById,
+  remove,
+  save,
+  getEmptyBook,
+  getNextBookId,
+  getFilterBy,
+  setFilterBy,
+  getDefaultFilter,
+  addReview,
+  deleteReview,
 }
 
 function query(filterBy = {}) {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            if (!books || !books.length) {
-                books = staticBooks;
-                utilService.saveToStorage(BOOK_KEY, books);
-            }
-            if (filterBy.title) {
-                const regex = new RegExp(filterBy.title, 'i')
-                books = books.filter(book => regex.test(book.title))
-            }
-            if (filterBy.price) {
-                books = books.filter(book => book.listPrice.amount <= filterBy.price)
-            }
-            return books
-        })
+  return storageService.query(BOOK_KEY).then((books) => {
+    if (!books || !books.length) {
+        books = staticBooks.map(book => ({
+            ...book,
+            reviews: _generateInitialReviews()
+          }));
+      utilService.saveToStorage(BOOK_KEY, books)
+    }
+    if (filterBy.title) {
+      const regex = new RegExp(filterBy.title, 'i')
+      books = books.filter((book) => regex.test(book.title))
+    }
+    if (filterBy.price) {
+      books = books.filter((book) => book.listPrice.amount <= filterBy.price)
+    }
+    return books
+  })
 }
 
 function getById(bookId) {
-    return storageService.get(BOOK_KEY, bookId)
+  return storageService.get(BOOK_KEY, bookId)
 }
 
 function remove(bookId) {
-    return storageService.remove(BOOK_KEY, bookId)
+  return storageService.remove(BOOK_KEY, bookId)
 }
 
 function save(book) {
-    if (book.id) {
-      return storageService.put(BOOK_KEY, book)
-    } else {
-      const newBook = _createBook(book.title, book.amount)
-      return storageService.post(BOOK_KEY, newBook)
-    }
+  if (book.id) {
+    return storageService.put(BOOK_KEY, book)
+  } else {
+    const newBook = _createBook(book.title, book.amount)
+    return storageService.post(BOOK_KEY, newBook)
   }
+}
 
 //   function getEmptyBook() {
 //     return { title: "", amount: "" }
 //   }
 
 function getEmptyBook() {
-    return {
-      id: '',
-      title: '',
-      subtitle: '',
-      authors: [],
-      publishedDate: 0,
-      description: '',
-      pageCount: 0,
-      categories: [],
-      thumbnail: '',
-      language: 'en',
-      listPrice: {
-        amount: 0,
-        currencyCode: 'USD',
-        isOnSale: false
-      }
-    }
+  return {
+    id: '',
+    title: '',
+    subtitle: '',
+    authors: [],
+    publishedDate: 0,
+    description: '',
+    pageCount: 0,
+    categories: [],
+    thumbnail: '',
+    language: 'en',
+    listPrice: {
+      amount: 0,
+      currencyCode: 'USD',
+      isOnSale: false,
+    },
   }
+}
 
 function getFilterBy() {
-    return { ...gFilterBy }
+  return { ...gFilterBy }
 }
 
 function setFilterBy(filterBy = {}) {
-    if (filterBy.txt !== undefined) gFilterBy.txt = filterBy.txt
-    if (filterBy.minAmount !== undefined) gFilterBy.minAmount = filterBy.minAmount
-    return gFilterBy
+  if (filterBy.txt !== undefined) gFilterBy.txt = filterBy.txt
+  if (filterBy.minAmount !== undefined) gFilterBy.minAmount = filterBy.minAmount
+  return gFilterBy
 }
 
 function getDefaultFilter() {
-    return { txt: '', minAmount: '' }; // Example default filter values
+  return { txt: '', minAmount: '' } // Example default filter values
 }
 
 function getNextBookId(bookId) {
-    return storageService.query(BOOK_KEY)
-        .then(books => {
-            let nextBookIdx = books.findIndex(book => book.id === bookId) + 1
-            if (nextBookIdx === books.length) nextBookIdx = 0
-            return books[nextBookIdx].id
-        })
+  return storageService.query(BOOK_KEY).then((books) => {
+    let nextBookIdx = books.findIndex((book) => book.id === bookId) + 1
+    if (nextBookIdx === books.length) nextBookIdx = 0
+    return books[nextBookIdx].id
+  })
 }
 
-
 function _createBook(title, amount) {
-    const randomIndex = utilService.getRandomIntInclusive(1, 20)
-    return {
-      id: utilService.makeId(),
-      title,
-      subtitle: utilService.makeLorem(15),
-      authors: ["Duna elle"],
-      publishedDate: utilService.getRandomIntInclusive(1700, 2022),
-      description: utilService.makeLorem(50),
-      pageCount: utilService.getRandomIntInclusive(1, 700),
-      categories: ["Computers", "Hack"],
-      thumbnail: `https://www.coding-academy.org/books-photos/${randomIndex}.jpg`,
-      language: "en",
-      listPrice: {
-        amount,
-        currencyCode: "EUR",
-        isOnSale: false,
-      },
-    }
+  const randomIndex = utilService.getRandomIntInclusive(1, 20)
+  return {
+    id: utilService.makeId(),
+    title,
+    subtitle: utilService.makeLorem(15),
+    authors: ['Duna elle'],
+    publishedDate: utilService.getRandomIntInclusive(1700, 2022),
+    description: utilService.makeLorem(50),
+    pageCount: utilService.getRandomIntInclusive(1, 700),
+    categories: ['Computers', 'Hack'],
+    thumbnail: `https://www.coding-academy.org/books-photos/${randomIndex}.jpg`,
+    language: 'en',
+    listPrice: {
+      amount,
+      currencyCode: 'EUR',
+      isOnSale: false,
+    },
   }
+}
 
-  function addReview(bookId, review) {
-    return getById(bookId)
-      .then(book => {
-        if (!book.reviews) book.reviews = [];
-        review.id = utilService.makeId();
-        book.reviews.push(review);
-        return save(book);
-      });
-  }
-  
-  function deleteReview(bookId, reviewId) {
-    return getById(bookId)
-      .then(book => {
-        book.reviews = book.reviews.filter(review => review.id !== reviewId);
-        return save(book);
-      });
-  }
+function addReview(bookId, review) {
+  return getById(bookId).then((book) => {
+    if (!book.reviews) book.reviews = []
+    review.id = utilService.makeId()
+    book.reviews.push(review)
+    return save(book)
+  })
+}
+
+function deleteReview(bookId, reviewId) {
+  return getById(bookId).then((book) => {
+    book.reviews = book.reviews.filter((review) => review.id !== reviewId)
+    return save(book)
+  })
+}
+
+function _generateInitialReviews() {
+  return [
+    {
+      id: utilService.makeId(),
+      fullname: utilService.getRandomName(),
+      rating: utilService.getRandomIntInclusive(1, 5),
+      readAt: _getRandomReviewDate(),
+    },
+    {
+      id: utilService.makeId(),
+      fullname: utilService.getRandomName(),
+      rating: utilService.getRandomIntInclusive(1, 5),
+      readAt: _getRandomReviewDate(),
+    },
+    {
+      id: utilService.makeId(),
+      fullname: utilService.getRandomName(),
+      rating: utilService.getRandomIntInclusive(1, 5),
+      readAt: _getRandomReviewDate(),
+    },
+  ]
+}
+
+function _getRandomReviewDate() {
+  const start = new Date(2021, 0, 1).getTime()
+  const end = new Date(2023, 0, 1).getTime()
+  const randomDate = new Date(utilService.getRandomIntInclusive(start, end))
+  return randomDate.toISOString().split('T')[0]
+}
 
 // function _createBooks() {
 //     let books = utilService.loadFromStorage(BOOK_KEY)
@@ -186,11 +214,10 @@ function _createBook(title, amount) {
 //     console.log('books', books)
 // }
 
-
 function _initializeBooks() {
-    let books = utilService.loadFromStorage(BOOK_KEY);
-    if (!books || !books.length) {
-        books = staticBooks;
-        utilService.saveToStorage(BOOK_KEY, books);
-    }
+  let books = utilService.loadFromStorage(BOOK_KEY)
+  if (!books || !books.length) {
+    books = staticBooks
+    utilService.saveToStorage(BOOK_KEY, books)
+  }
 }
--- react-template-proj/services/util.service.js
+++ react-template-proj/services/util.service.js
@@ -1,63 +1,115 @@
 export const utilService = {
-    makeId,
-    makeLorem,
-    getRandomIntInclusive,
-    loadFromStorage,
-    saveToStorage,
-    padNum,
-    getDayName,
-    getMonthName
+  makeId,
+  makeLorem,
+  getRandomIntInclusive,
+  loadFromStorage,
+  saveToStorage,
+  padNum,
+  getDayName,
+  getMonthName,
+  getRandomName,
 }
 
 function makeId(length = 6) {
-    var genId = ""
-    var possible =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  
-    for (var i = 0; i < length; i++) {
-      genId += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-  
-    return genId
+  var genId = ''
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  for (var i = 0; i < length; i++) {
+    genId += possible.charAt(Math.floor(Math.random() * possible.length))
   }
 
+  return genId
+}
+
 function makeLorem(size = 100) {
-    var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
-    var txt = ''
-    while (size > 0) {
-        size--
-        txt += words[Math.floor(Math.random() * words.length)] + ' '
-    }
-    return txt
+  var words = [
+    'The sky',
+    'above',
+    'the port',
+    'was',
+    'the color of television',
+    'tuned',
+    'to',
+    'a dead channel',
+    '.',
+    'All',
+    'this happened',
+    'more or less',
+    '.',
+    'I',
+    'had',
+    'the story',
+    'bit by bit',
+    'from various people',
+    'and',
+    'as generally',
+    'happens',
+    'in such cases',
+    'each time',
+    'it',
+    'was',
+    'a different story',
+    '.',
+    'It',
+    'was',
+    'a pleasure',
+    'to',
+    'burn',
+  ]
+  var txt = ''
+  while (size > 0) {
+    size--
+    txt += words[Math.floor(Math.random() * words.length)] + ' '
+  }
+  return txt
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function saveToStorage(key, value) {
-    localStorage.setItem(key, JSON.stringify(value))
+  localStorage.setItem(key, JSON.stringify(value))
 }
 
 function loadFromStorage(key) {
-    const data = localStorage.getItem(key)
-    return (data) ? JSON.parse(data) : undefined
+  const data = localStorage.getItem(key)
+  return data ? JSON.parse(data) : undefined
 }
 
 function padNum(num) {
-    return (num + '').padStart(2, '0')
+  return (num + '').padStart(2, '0')
 }
 
 function getDayName(date, locale) {
-    date = new Date(date)
-    return date.toLocaleDateString(locale, { weekday: 'long' })
+  date = new Date(date)
+  return date.toLocaleDateString(locale, { weekday: 'long' })
 }
 
 function getMonthName(date) {
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-    ]
-    return monthNames[date.getMonth()]
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  return monthNames[date.getMonth()]
+}
+
+function getRandomName() {
+  const firstNames = ['John', 'Jane', 'Alex', 'Emily', 'Chris', 'Katie', 'Michael', 'Laura', 'David', 'Sarah']
+  const lastNames = ['Doe', 'Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis', 'Garcia', 'Martinez']
+  const firstName = firstNames[Math.floor(Math.random() * firstNames.length)]
+  const lastName = lastNames[Math.floor(Math.random() * lastNames.length)]
+  return `${firstName} ${lastName}`
 }
